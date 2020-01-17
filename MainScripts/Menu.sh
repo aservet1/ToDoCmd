836c18794e6d59efad98df39f10dbedc3ff74324
Alejandro Servetto <aservet1@binghamton.edu>
2020-01-17 13:15:50 -0500

    made viewing and choosing better by implementing arrays and selecting by index

--- MainScripts/Menu.sh
+++ MainScripts/Menu.sh
@@ -5,8 +5,7 @@ function ask_view_all
 	echo 'would you like to see the existing files?'
 	read response
 	if [ $response == 'y' ]
-	then
-		echo ''
+	then	echo ''
 		view_all
 		echo ''
 	fi
@@ -14,12 +13,11 @@ function ask_view_all
 
 function view_all()
 {
-	array=($(ls *.todo))
-	for item in ${array[@]}
-	do
-		printf ${item%'.todo'}"\t"
+	array=($(ls))
+	for i in ${!array[@]}
+	do	n=$(( i+1 ))
+		echo "($n) ${array[i]%'.todo'}"
 	done
-	printf '\n'
 }
 
 function make_new()
@@ -28,15 +26,12 @@ function make_new()
 	echo 'enter file name to create'
 	read filename
 	if [ -f $filename.todo ]
-	then
-		echo $filename' already exists. try again?'
+	then	echo $filename' already exists. try again?'
 		read response
 		if [ $response == 'y' ]
-			then make_new # the recursion causes lag in nano-ing the file once you make a valid one
+		then make_new ### recursion is dumb
 		fi
-
-	else
-		touch $filename.todo
+	else	touch $filename.todo
 		nano $filename.todo
 	fi
 }
@@ -47,15 +42,13 @@ function rename()
 	echo 'enter the name of the file you would like to fix'
 	read oldname
 	if [ -f $oldname.todo ]
-	then
-		echo 'enter the name you want to change it to'
+	then	echo 'enter the name you want to change it to'
 		read newname
 		mv $oldname.todo $newname.todo
-	else
-		echo 'no file found named '$oldname' . try again?'
+	else	echo 'no file found named '$oldname' . try again?'
 		read response
 		if [ $response == 'y' ]
-			then rename
+		then	rename ### recursion is dumb
 		fi
 	fi
 }
@@ -63,60 +56,104 @@ function rename()
 function edit_lists()
 {
 	ask_view_all
-	echo 'enter the name of the list you would like to edit'
-	read toedit
-	if [ -f $toedit.todo ]
-	then
-		nano $toedit.todo
-		../MainScripts/UpdateMasterToDo.sh
-	else
-		echo $toedit ' does not exist'
-		read response
-		if [ respose == 'y' ]
-			then edit_lists
+	lists=($(ls *.todo)) ### find a way to exclude meta-lists
+
+	echo 'enter the ones to edit by name or number. * for all'
+	read editrequests
+	if [ "$editrequests" == '*' ]; then editrequests=${!lists[@]}; fi
+
+	for entry in $editrequests
+	do	if [[ $entry =~ ^[0-9]+$ ]]
+		then	n=$entry
+			i=$(( $n-1 ))
+			toedit="$toedit ${lists[i]}"
+		else	toedit="$toedit ${entry}.todo"
 		fi
-	fi
+	done
+	nano $toedit
+
+	../../MainScripts/UpdateMasterToDo.sh .
 }
 
 function delete()
 {
 	ask_view_all
+	lists=($(ls *.todo))
 	echo 'enter the name of the file you would like to delete'
 	read todelete
+	if [[ $todelete =~ ^[0-9]+$ ]]
+	then	n=$todelete
+		i=$(( n-1 ))
+		todelete=${lists[i]%'.todo'}
+	fi
+	
 	if [ -f $todelete.todo ]
-	then
-		echo 'are you sure? this cannot be undone! at all!'
+	then	echo 'are you sure? this cannot be undone! at all!'
 		read response
 		if [ $response == 'y' ]
 			then rm $todelete.todo
 		fi
-	else
-		echo $todelete' does not exist. try again?'
+	else	echo $todelete' does not exist. try again?'
 		read response
 		if [ $response == 'y' ]
-			then delete
+		then delete ### recursion is dumb
 		fi
 	fi
 }
 
-function main()
+function in_category()
 {
-	MainScripts = $
-	cd ../ListStorage/Fall2019 #for now...need to have a way to choose which category and also make new categories
-	echo 'hello. your options: '
-	echo '(1) - see existing list'
+	category=$1
+	cd $category
+	echo 'in category '$category'. your options: '
+	echo '(1) - see existing lists'
 	echo '(2) - make new list'
 	echo '(3) - rename a list'
 	echo '(4) - edit list(s)'
 	echo '(5) - delete a list'
 	read input
+
+#	while [[ $input !=~ "^[1-5]$" ]]
+#	do	echo 'invalid entry: '$input
+#		printf 'try again: '
+#		read input
+#	done
+
 	echo ''
 	if   [ $input == 1 ]; then view_all
 	elif [ $input == 2 ]; then make_new
 	elif [ $input == 3 ]; then rename
-	elif [ $input == 4 ];  then edit_lists
+	elif [ $input == 4 ]; then edit_lists
 	elif [ $input == 5 ]; then delete
-	else echo 'no valid entry'; fi
+	fi
+}
+
+function main()
+{
+	cd ../ListStorage
+	view_all
+	echo 'pick a category by name or number'
+#	categories=($(ls))
+#	for i in ${!categories[@]}
+#	do	n=$(( $i+1 ))
+#		echo $n ${categories[i]}
+#	done
+
+	read category ### gotta validate entries
+
+	if [[ $category =~ ^[0-9]+$ ]]
+	then	n=$category
+		i=$(( $n-1 ))
+		category=${categories[i]}
+	fi
+
+#	while [ ! -d $category ]
+#	do	echo 'invalid category: '$category
+#		printf 'try again: '
+#		read category
+#	done
+
+	in_category $category
 }
 
 main
